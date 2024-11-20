aefd216ddeff76b3ee04faf50554902ab197ab0f
Nahiyan-Alam <alamnahiyan.info@gmail.com>
2024-11-20 19:09:34 +0100

    Add files via upload

---
+++ FriendlyChatbot.py
@@ -0,0 +1,26 @@
+name = input("Hi there! What's your name? ")
+print("Good day, " + name + "!")
+
+age = input("How old are you, " + name + "? ")
+print("Oh, that's so nice!")
+
+location = input("Where do you live, " + name + "? ")
+print("Wow, I live in " + location + " too!")
+
+surname = input("I know your first name, but not your last name. What's your last name? ")
+print("Your names are so beautiful, " + name + " " + surname + ". Amazing!")
+
+print("So, your full name is " + name + " " + surname + ".")
+
+input("Let’s continue, shall we? ")
+
+school = input("Which school do you go to? ")
+print("Oh, interesting! My dad is a professor at a school!")
+
+professor = input("Do you know my dad? His name is Alex: ")
+print("That’s cool!")
+
+print("I have some Python work to do.")
+input("See you next time. Is that okay? ")
+print("Alright, goodbye " + name + " " + surname + "!")
+print("Take care, " + name + "!")
